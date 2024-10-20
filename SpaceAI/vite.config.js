5d059d67269315fb6f18c15dcc65e0304c3bbcd7
RoshanDileep <118088610+RoshanDileep@users.noreply.github.com>
2024-10-20 14:05:37 -0700

    Reverted

--- SpaceAI/vite.config.js
+++ SpaceAI/vite.config.js
@@ -1,19 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { resolve } from 'path'
+import {resolve} from 'path'
 
 // https://vitejs.dev/config/
-const root = resolve(__dirname, 'src') // Points to the src directory
 export default defineConfig({
-  root,
+  
   plugins: [react()],
-  build: {
-    emptyOutDir: true,
-    rollupOptions: {
-      input: {
-        main: resolve(root, 'index.html'), // Points to src/index.html
-        login: resolve(root, 'login', 'index.html'), // Points to src/about/index.html
-      }
-    }
-  }
+ 
 })
+
